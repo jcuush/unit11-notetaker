1477cb2421b54bee74ebbb8aed0a9c55ca7ff648
jcuush <jrcushing@sbcglobal.net>
2020-10-25 21:39:28 -0500

    set up server routes

--- Develop/public/assets/server.js
+++ Develop/public/assets/server.js
@@ -9,6 +9,33 @@ const app = express();
 // Sets an initial port.
 const PORT = process.env.PORT || 8080;
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Routes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public.index.html"));
+  });
+
+app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
+        if(err) {
+            console.log("Error");
+        }
+        else {
+            console.log(data);
+            res.json(JSON.parse(data));
+        }
+    })
+});
+
 // Server begins to listen
 app.listen(PORT, function() {
     console.log("Server is listening");
